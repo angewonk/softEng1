1afb1a68d5e7d8f8cad1562b292a4df2a2312736
angewo gerawrd <angelomallari1123.com>
2023-08-26 10:29:14 +0800

    adding some changes to Hello.java

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello{
   System.out.println("Hello, Software Engineering Course!");
   System.out.println("My name is MALLARI, Angelo Gerard T.!");
   System.out.println("That is why developers are highly-paid!");
+  System.out.println ("Complicated world of SoftDev that is why it pays much!");
   }
 }
